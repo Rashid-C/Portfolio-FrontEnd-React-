a3e8958efd433414f9b6e8c6897ce284e65de0e0
Rashid-C <anurashid105@gmail.com>
2026-03-07 14:14:52 +0400

    update 3

--- rashid-c/src/components/ProjectCard.jsx
+++ rashid-c/src/components/ProjectCard.jsx
@@ -12,7 +12,7 @@ const ProjectCard = ({ imgSrc, title, tags, projectLink, classes }) => {
   return (
     <div
       className={
-        'relative p-4 rounded-2xl bg-zinc-800 hover:bg-zinc-700/50 active:bg-zinc-700/60 ring-1 ring-inset ring-zinc-50/5 transition-colors ' +
+        'relative h-full p-4 rounded-2xl bg-zinc-800 hover:bg-zinc-700/50 active:bg-zinc-700/60 ring-1 ring-inset ring-zinc-50/5 transition-colors ' +
         classes
       }
     >
@@ -20,11 +20,11 @@ const ProjectCard = ({ imgSrc, title, tags, projectLink, classes }) => {
         <img src={imgSrc} alt={title} loading='lazy' className='img-cover' />
       </figure>
 
-      <div className='flex items-center justify-between gap-4'>
-        <div>
+      <div className='flex items-start justify-between gap-3'>
+        <div className='min-w-0 flex-1'>
           <h3 className='title-1 mb-3'>{title}</h3>
 
-          <div className='flex flex-wrap items-center'>
+          <div className='flex flex-wrap items-center gap-2'>
             {tags.map((label, key) => (
               <span
                 key={key}
@@ -36,7 +36,7 @@ const ProjectCard = ({ imgSrc, title, tags, projectLink, classes }) => {
           </div>
         </div>
 
-        <div className='w-11 h-11 rounded-lg grid place-items-center bg-sky-400 text-zinc-950 shrink-0'>
+        <div className='w-11 h-11 rounded-lg grid place-items-center bg-sky-400 text-zinc-950 shrink-0 self-start'>
           <span className='material-symbols-rounded' aria-hidden='true'>
             arrow_outward
           </span>
